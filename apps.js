e6a9b411efed1ae9b3c9369e0b2c8ebd1ca48b15
bzinsky <bzinsky@gmail.com>
2023-09-21 17:40:02 -0400

    updating apps

--- apps.js
+++ apps.js
@@ -16,3 +16,37 @@ const mainMenuChoices = [
   'Update an employee role',
   'Exit',
 ];
+
+async function mainMenu() {
+    const { choice } = await inquirer.prompt({
+      name: 'choice',
+      type: 'list',
+      message: 'Select an option:',
+      choices: mainMenuChoices,
+    });
+  
+    switch (choice) {
+      case 'View all departments':
+        await viewAllDepartments();
+        break;
+      case 'View all roles':
+        await viewAllRoles();
+        break;
+      case 'View all employees':
+        // Call the function to view all employees
+        break;
+      case 'Add a department':
+        await addDepartmentPrompt();
+        break;
+      // ... (Other cases)
+      case 'Exit':
+        console.log('Goodbye!');
+        process.exit(0);
+      default:
+        console.log('Invalid choice.');
+        break;
+    }
+  
+    // Return to the main menu after an action is performed
+    mainMenu();
+  }
